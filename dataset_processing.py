c9c63be918a118054c2419346e3e4c49624c47f3
Chayne Thrash <chaynethrash@gmail.com>
2021-04-25 09:55:19 -0400

    Limited the embeddings to only contain tokens in the training data and the unlabelled data

--- dataset_processing.py
+++ dataset_processing.py
@@ -6,13 +6,17 @@ from emoji import EMOJI_UNICODE_ENGLISH
 import unicodedata
 
 
-def read_twitter_stock_data(file_path):
+def read_twitter_stock_data(file_path, remove_unlabelled_rows=True):
     with open(file_path, "r", encoding="utf-8") as f:
         file_text = f.read()
 
         file_text = html.unescape(file_text)
         df = pandas.read_csv(StringIO(file_text), sep=";")
-        return df[df["sentiment"].notnull()]
+
+        if remove_unlabelled_rows:
+            return df[df["sentiment"].notnull()]
+        else:
+            return df
 
 
 class Tokenizer():
@@ -26,12 +30,14 @@ class Tokenizer():
         self.max_emoji_len = max([len(e) for e in self.emojis])
 
     def tokenize(self, text):
+        text = self.preprocess_text(text)
         current_token = ""
         tokens = []
 
         i = 0
         while i < len(text):
             c = text[i]
+
             if self.word_char_regex.fullmatch(c):
                 current_token += c
             else:
@@ -83,14 +89,32 @@ class Tokenizer():
             tokens.append(current_token)
         return tokens
 
+    def preprocess_text(self, text):
+        text = text.lower()
 
+        url_regex = r'https?://\S+\b|www\.(\w+\.)+\S*'
+        user_regex = r'@\w+'
+        eyes = "[8:=;]"
+        nose = "['`-]?"
+        smiley_regex = "({0}{1}[)d]+|[)d]+{1}{0})".format(eyes, nose)
+        lol_face_regex = eyes + nose + "p+"
+        sad_face_regex = "({0}{1}\\(+|{1}{0}\\))".format(eyes, nose)
+        neutral_face_regex = "{0}{1}[\\/|l*]".format(eyes, nose)
+        number_regex = r'[-+]?[.\d]*[\d]+[:,.\d]*'
+        punct_repitition_regex = r'([!?.]){2,}'
+        word_ending_repitition_regex = r'\b(\S*?)(.)\2{2,}\b'
 
+        text = re.sub(r'\s{2,}', ' ', text)
+        text = re.sub(r'\'', '', text)
+        text = re.sub(url_regex, "<URL>", text)
+        text = re.sub(user_regex, "<USER>", text)
+        text = re.sub(smiley_regex, "<SMILE>", text)
+        text = re.sub(lol_face_regex, "<LOLFACE>", text)
+        text = re.sub(sad_face_regex, "<SADFACE>", text)
+        text = re.sub(neutral_face_regex, "<NEUTRALFACE>", text)
+        text = re.sub("<3", "<HEART>", text)
+        text = re.sub(number_regex, "<NUMBER>", text)
+        text = re.sub(punct_repitition_regex, lambda match: match.group(1) + " <REPEAT>", text)
+        text = re.sub(word_ending_repitition_regex, lambda match: match.group(1) + match.group(2) + " <ELONG>", text)
 
-# text = re.sub(url_regex, "<URL>", text)
-#     text = re.sub(user_regex, "<USER>", text)
-#     text = re.sub(smiley_regex, "<SMILE>", text)
-#     text = re.sub(lol_face_regex, "<LOLFACE>", text)
-#     text = re.sub(sad_face_regex, "<SADFACE>", text)
-#     text = re.sub(neutral_face_regex, "<NEUTRALFACE>", text)
-#     text = re.sub("<3", "<HEART>", text)
-#     text = re.sub(number_regex, "<NUMBER>", text)
+        return text
--- pretrained_embeddings.py
+++ pretrained_embeddings.py
@@ -16,32 +16,3 @@ def read_embeddings_from_file(file_path, word_whitelist=None):
 
     return embeddings
 
-
-def add_special_tokens_to_text(text):
-    text = text.lower()
-
-    url_regex = r'https?://\S+\b|www\.(\w+\.)+\S*'
-    user_regex = r'@\w+'
-    eyes = "[8:=;]"
-    nose = "['`-]?"
-    smiley_regex = "({0}{1}[)d]+|[)d]+{1}{0})".format(eyes, nose)
-    lol_face_regex = eyes + nose + "p+"
-    sad_face_regex = "({0}{1}\\(+|{1}{0}\\))".format(eyes, nose)
-    neutral_face_regex = "{0}{1}[\\/|l*]".format(eyes, nose)
-    number_regex = r'[-+]?[.\d]*[\d]+[:,.\d]*'
-    punct_repitition_regex = r'([!?.]){2,}'
-    word_ending_repitition_regex = r'\b(\S*?)(.)\2{2,}\b'
-
-    text = re.sub(r'\s{2,}', ' ', text)
-    text = re.sub(url_regex, "<URL>", text)
-    text = re.sub(user_regex, "<USER>", text)
-    text = re.sub(smiley_regex, "<SMILE>", text)
-    text = re.sub(lol_face_regex, "<LOLFACE>", text)
-    text = re.sub(sad_face_regex, "<SADFACE>", text)
-    text = re.sub(neutral_face_regex, "<NEUTRALFACE>", text)
-    text = re.sub("<3", "<HEART>", text)
-    text = re.sub(number_regex, "<NUMBER>", text)
-    text = re.sub(punct_repitition_regex, lambda match: match.group(1) + " <REPEAT>", text)
-    text = re.sub(word_ending_repitition_regex, lambda match: match.group(1) + match.group(2) + " <ELONG>", text)
-
-    return text
